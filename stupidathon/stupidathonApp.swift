40dab56ee9938b1976fc2dd130374841c2da1dc8
abby-73 <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2023-11-22 10:03:05 +0800

    Initial Commit

---
+++ stupidathon/stupidathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  stupidathonApp.swift
+//  stupidathon
+//
+//  Created by Abigail Lau on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct stupidathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
